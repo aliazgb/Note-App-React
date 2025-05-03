f178c37291aaba3d873fa1e9d89e5039c53e3e6c
Ali Azarkashb <ali.azgb10@gmail.com>
2025-05-04 00:01:37 +0200

    test: check if title input is empty after submitting a note

--- src/components/tests/NoteApp.test.jsx
+++ src/components/tests/NoteApp.test.jsx
@@ -1,15 +1,19 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
-import NoteApp from "../NoteApp";
 import { NotesProvider } from "../../context/NotesContext";
+import NoteApp from "../NoteApp";
 
-function addNote() {
-    
+function addNote(note) {
+  const inputTitle = screen.getByPlaceholderText(/Note title/i);
+  const inputDesc = screen.getByPlaceholderText(/Note description/i);
+  const button = screen.getByRole("button", { name: /Add New Note/i });
+
+  note.forEach((n) => {
+    fireEvent.change(inputTitle, { target: { value: n.title } });
+    fireEvent.change(inputDesc, { target: { value: n.des } });
+    fireEvent.click(button);
+  });
 }
-
-
-
-
 
 test("Note App #1: should input be empty after submit", () => {
   render(
@@ -17,11 +21,14 @@ test("Note App #1: should input be empty after submit", () => {
       <NoteApp sortBy={"latest"} />
     </NotesProvider>
   );
+
   const inputTitle = screen.getByPlaceholderText(/Note title/i);
-  const inputDesc = screen.getByPlaceholderText(/Note description/i);
-  fireEvent.change(inputTitle, { target: { value: "hello kos" } });
-  fireEvent.change(inputDesc, { target: { value: "hello kon" } });
-  const button = screen.getByRole("button", { name: /Add New Note/i });
-  fireEvent.click(button);
   expect(inputTitle.value).toBe("");
+
+  addNote([
+    {
+      title: "Hello this is test title",
+      des: "Hello this is test description",
+    },
+  ]);
 });
